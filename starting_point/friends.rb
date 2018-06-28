9c1872189c38fc8536f6bbe5503b0b24a4f86fd2
Joanna Gora <joanna.gora@protonmail.com>
2018-06-28 14:49:57 +0100

    Completed first two tests

---
+++ starting_point/friends.rb
@@ -0,0 +1,18 @@
+
+
+def get_name(person)
+  return person[:name]
+end
+
+
+def return_fav_tv_show(person)
+  return person[:favourites][:tv_show]
+end
+
+def likes_food(person, food)
+  if person[:favourites][:snacks][0] == food
+    return true
+  else
+    return false
+  end
+end
